a6f25f051bacf4bc1090276acd4a539ceede3116
zeculesu <artiyjana@yandex.ru>
2026-02-05 05:43:00 +0300

    step 2

--- source/vtfs.c
+++ source/vtfs.c
@@ -1,6 +1,7 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/mount.h>
 
 #define MODULE_NAME "vtfs"
 
@@ -10,12 +11,86 @@ MODULE_DESCRIPTION("A simple FS kernel module");
 
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
 
+struct dentry* vtfs_mount(
+    struct file_system_type* fs_type,
+    int flags,
+    const char* token,
+    void* data
+);
+void vtfs_kill_sb(struct super_block* sb);
+int vtfs_fill_super(struct super_block *sb, void *data, int silent);
+struct inode* vtfs_get_inode(
+  struct super_block* sb, 
+  const struct inode* dir, 
+  umode_t mode, 
+  int i_ino
+);
+
+struct dentry* vtfs_mount(
+  struct file_system_type* fs_type,
+  int flags,
+  const char* token,
+  void* data
+) {
+  struct dentry* ret = mount_nodev(fs_type, flags, data, vtfs_fill_super);
+  if (ret == NULL) {
+    printk(KERN_ERR "Can't mount file system");
+  } else {
+    printk(KERN_INFO "Mounted successfuly");
+  }
+  return ret;
+}
+
+struct inode* vtfs_get_inode(
+  struct super_block* sb, 
+  const struct inode* dir, 
+  umode_t mode, 
+  int i_ino
+) {
+  struct inode *inode = new_inode(sb);
+  if (!inode) return NULL;
+
+  inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
+  inode->i_ino = i_ino;
+  
+  return inode;
+}
+
+struct file_system_type vtfs_fs_type = {
+  .name = "vtfs",
+  .mount = vtfs_mount,
+  .kill_sb = vtfs_kill_sb,
+};
+
+void vtfs_kill_sb(struct super_block* sb) {
+  printk(KERN_INFO "vtfs super block is destroyed. Unmount successfully.\n");
+}
+
+int vtfs_fill_super(struct super_block *sb, void *data, int silent) {
+  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR, 1000);
+
+  sb->s_root = d_make_root(inode);
+  if (sb->s_root == NULL) {
+    return -ENOMEM;
+  }
+
+  printk(KERN_INFO "return 0\n");
+  return 0;
+}
+
 static int __init vtfs_init(void) {
+  int ret = register_filesystem(&vtfs_fs_type);
+  if (ret != 0) {
+      pr_err("[vtfs] failed to register filesystem\n");
+      return ret;
+  }
+  
   LOG("VTFS joined the kernel\n");
   return 0;
 }
 
 static void __exit vtfs_exit(void) {
+  unregister_filesystem(&vtfs_fs_type);
   LOG("VTFS left the kernel\n");
 }
 
